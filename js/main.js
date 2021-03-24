173c67315907f6b0ec82cd04aec3cd682c628ce6
tanigawahideaki <80952139+tanigawahideaki@users.noreply.github.com>
2021-03-24 19:37:01 +0900

    Update main.js
    まだ完成していません

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+(() => {
 
+    //ここに命令を書く
+    const $doc = document; //変数にdocumentという文字列を代入
+    const $tab = document.getElementById('js-tab'); //idがjs-tabの情報を変数に代入
+    const $nav = $tab.querySelectorAll('[data-nav]'); //指定したセレクタに一致する最初のHTML要素(Element)を順に取得する
+    const $content = $tab.querySelectorAll('[data-content]');
+
+    //初期化
+    const init = () => {
+        $content[0].style.display = 'block'; //"タブその0"だけを表示させる
+    };
+
+    init();
+
+    //ここからクリック実績にタブが切り替わるという命令を書いていく
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log(e);
+    };
+
+    $nav[0].addEventListener('click', (e) => handleClick(e));
+
+})();
